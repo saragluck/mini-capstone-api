fe2a6f9b8aaa1769b0512caba3af079cf81856a9
Sara Gluck <srfgglck@gmail.com>
2022-11-02 14:03:56 -0500

    Create a single route to choose product based on ID

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,21 +1,6 @@
 class ProductsController < ApplicationController
-  def all_products
-    all_products = Product.all
-    render json: all_products.as_json
-  end
-
-  def apple
-    apple = Product.first
-    render json: apple.as_json
-  end
-
-  def corn
-    corn = Product.second
-    render json: corn.as_json
-  end
-
-  def pomegranate
-    pomegranate = Product.third
-    render json: pomegranate.as_json
+  def product
+    product = Product.find_by(id: params["id"])
+    render json: product.as_json
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,5 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  get "/all_products", controller: "products", action: "all_products"
-  
-  get "/apple", controller: "products", action: "apple"
-  get "/corn", controller: "products", action: "corn"
-  get "/pomegranate", controller: "products", action: "pomegranate"
+  get "/product/:id", controller: "products", action: "product"
 end
