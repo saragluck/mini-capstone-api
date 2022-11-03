28e324b9e1ba71740448469940f8b14b20746ffd
Sara Gluck <srfgglck@gmail.com>
2022-11-03 13:45:34 -0500

    Modified products create action to use body params

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -10,7 +10,7 @@ class ProductsController < ApplicationController
   end
 
   def create
-    products = Product.new(name: "Avocado", price: 0.68, image_url: "https://images.heb.com/is/image/HEBGrocery/000318627", description: "Officially a fruit, you can use it to make guacamole or avocado toast")
+    products = Product.new(name: params["name"], price: params["price"], image_url: params["image_url"], description: params["description"])
     products.save
     render json: products.as_json
   end
