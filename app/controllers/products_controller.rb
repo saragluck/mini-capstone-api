c3b73cc08e7a86a6ded40cff978b968f350ea079
Sara Gluck <srfgglck@gmail.com>
2022-11-07 11:54:38 -0600

    Adds a method to show created_at time in a user-friendly manner

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,7 +6,7 @@ class ProductsController < ApplicationController
 
   def show
     products = Product.find_by(id: params["id"])
-    render json: products.as_json
+    render json: products.as_json(methods: [:friendly_created_at])
   end
 
   def create
--- mini-capstone-frontend.rb
+++ mini-capstone-frontend.rb
@@ -1,2 +1,3 @@
 require "http"
 clear
+puts "Press 1 to see all products"
