cd054ec7c422ec7794d56ef525226c0e564bcb60
Sara Gluck <srfgglck@gmail.com>
2022-11-07 12:36:27 -0600

    Adds is_discounted? model method and adds to show json, adds gem jb in the gemfile

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,10 @@
 class Product < ApplicationRecord
-
-  def friendly_created_at 
+  def friendly_created_at
     created_at.strftime("%B %e, %Y")
+  end
+
+  def is_discounted?
+    price <= 10
   end
   
 end
