0917fa8f29c0904aadaafaee1ce6ad5af22f17e2
Sara Gluck <srfgglck@gmail.com>
2022-11-08 13:09:24 -0600

    Adds validations

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,11 @@
 class Product < ApplicationRecord
+  validates :name, presence: true
+  validates :name, uniqueness: true
+  validates :price, presence: true
+  validates :price, numericality: true
+  validates :price, comparison: { greater_than: 0 }
+  validates :description, length: { in: 10..500 }
+
   def friendly_created_at
     created_at.strftime("%B %e, %Y")
   end
@@ -6,5 +13,4 @@ class Product < ApplicationRecord
   def is_discounted?
     price <= 10
   end
-  
 end
