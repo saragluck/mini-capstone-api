7adccf89ea5887619f89def1c2baed1cc95c61a3
Sara Gluck <srfgglck@gmail.com>
2022-11-08 11:54:43 -0600

    Changes data type of decription column of my database from string to text

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,7 @@
 class ProductsController < ApplicationController
   def index
-    products = Product.all
-    render json: products.as_json
+    @products = Product.all
+    render templace: "products/index"
   end
 
   def show
@@ -16,15 +16,15 @@ class ProductsController < ApplicationController
   end
 
   def update
-    products = Product.find_by(id: params["id"])
+    @products = Product.find_by(id: params["id"])
 
-    products.name = params["name"] || products.name
-    products.price = params["price"] || products.price
-    products.image_url = params["image_url"] || products.image_url
-    products.description = params["description"] || products.description
+    @products.name = params["name"] || @products.name
+    @products.price = params["price"] || @products.price
+    @products.image_url = params["image_url"] || @products.image_url
+    @products.description = params["description"] || @products.description
 
-    products.save
-    render json: products.as_json
+    @products.save
+    render :show
   end
 
   def destroy
