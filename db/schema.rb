febd0e8bee91336b0640d2a822d6a6323093e2f0
Sara Gluck <srfgglck@gmail.com>
2022-11-08 12:32:21 -0600

    Fixes a mistake I made in changing price to decimal

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_08_175529) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_08_183018) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,6 +21,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_08_175529) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "stock"
   end
 
 end
