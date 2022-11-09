b1b3428ae06cf0fe15d144ae3501cb00d4ceae8a
Sara Gluck <srfgglck@gmail.com>
2022-11-09 12:29:07 -0600

    Creates migration to add supplier_id column to products table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_08_183018) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_09_175749) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -22,6 +22,14 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_08_183018) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "stock"
+  end
+
+  create_table "suppliers", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "phone_number"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,7 @@
 product = Product.create(name: "Apple", price: 1.40, image_url: "https://usapple.org/wp-content/uploads/2019/10/apple-pink-lady.png", description: "An edible fruit from an apple tree that is red and crunchy")
 product = Product.create(name: "Corn", price: 0.50, image_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQhNehPr_vsIRfEOjhzw-fM2TBHL3HMILoKiw&usqp=CAU", description: "It's corn!")
 product = Product.create(name: "Pomegranate", price: 2.48, image_url: "https://www.news-medical.net/image.axd?picture=2020%2F12%2Fshutterstock_1291478515.jpg", description: "A plant to which one can be slightly allergic")
+
+supplier = Supplier.create!(name: "Pomegranate person", email: "pomegranateperson@gmail.com", phone_number: "1234567890")
+supplier = Supplier.create!(name: "Organic supplier", email: "organicsupplier@gmail.com", phone_number: "3475339035")
+supplier = Supplier.create!(name: "Regular supplier", email: "regularsupplier@gmail.com", phone_number: "9490285904")
