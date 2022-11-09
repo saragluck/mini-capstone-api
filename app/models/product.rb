024b8442f3242d2725b9af5b3a89c1a1e5d4cf3d
Sara Gluck <srfgglck@gmail.com>
2022-11-09 12:56:37 -0600

    Adds supplier table and association

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,6 +5,10 @@ class Product < ApplicationRecord
   validates :price, numericality: { greater_than: 0 }
   validates :description, length: { in: 10..500 }
 
+  def supplier
+    Supplier.find_by(id: supplier_id) #The ID of the supplier (in the supplier table) should match the supplier_id in my products table.
+  end
+
   def friendly_created_at
     created_at.strftime("%B %e, %Y")
   end
